40a5aaa7436ce30a8f50fec29f2871d9709f08ce
Srikant <kumar_srikant@outlook.com>
2018-06-10 19:42:19 +0530

    Added new forms.py and made changes to the models and templates files

--- budget/models.py
+++ budget/models.py
@@ -10,6 +10,18 @@ class Project(models.Model):
 		self.slug = slugify(self.name)
 		super(Project, self).save(*args, **kwargs)
 
+	def budget_left(self):
+		expense_list = Expense.objects.filter(project=self)
+		total_expense_amount = 0
+		for expense in expense_list:
+			total_expense_amount += expense.amount
+
+		return self.budget - total_expense_amount
+
+	def total_transaction(self):
+		expense_list = Expense.objects.filter(project=self)
+		return len(expense_list)
+
 	def __str__(self):
 		return self.name
 
